9b8fad8dca009543a3657f53f2cc4e54b9c664a8
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2024-08-19 18:20:12 +0600

    crate goal progress bar

--- lib/controllers/goal_controller.dart
+++ lib/controllers/goal_controller.dart
@@ -5,6 +5,7 @@ import '../pages/home_page/widgets/daily_goal/goal_class.dart';
 
 class GoalsController extends GetxController {
   static GoalsController get instance => Get.find();
+
   //date time format
   String formatDateTime(DateTime dateTime,
       {String dateFormat = 'E, MMM dd', String timeFormat = 'hh:mm a'}) {
@@ -102,4 +103,64 @@ class GoalsController extends GetxController {
       timeRemaining: "10 min remaining",
     ),
   ].obs;
+
+
+
+// Goal Progress Bar Data
+  RxString selectedTimeFrame = 'Weekly'.obs;
+
+  // Weekly, Monthly, and Yearly data
+  var weeklyData = [
+    [60, 40, 80], // Monday
+    [50, 60, 30], // Tuesday
+    [70, 50, 60], // Wednesday
+    [30, 40, 70], // Thursday
+    [80, 60, 50], // Friday
+    [60, 70, 40], // Saturday
+    [50, 30, 60], // Sunday
+  ].obs;
+
+  var monthlyData = [
+    [40, 50, 60],
+    [50, 40, 70],
+    [30, 60, 40],
+    [70, 50, 30],
+    [60, 70, 80],
+    [80, 60, 70],
+    [60, 30, 50],
+  ].obs;
+
+  var yearlyData = [
+    [50, 60, 70],
+    [70, 50, 80],
+    [60, 40, 50],
+    [30, 60, 40],
+    [80, 70, 60],
+    [50, 40, 60],
+    [60, 70, 80],
+  ].obs;
+
+  RxList<List<int>> displayedData = RxList<List<int>>([]);
+
+  @override
+  void onInit() {
+    super.onInit();
+    // Set initial data to weekly data
+    displayedData.assignAll(weeklyData);
+  }
+
+  void updateDisplayedData() {
+    if (selectedTimeFrame.value == 'Weekly') {
+      displayedData.assignAll(weeklyData);
+    } else if (selectedTimeFrame.value == 'Monthly') {
+      displayedData.assignAll(monthlyData);
+    } else if (selectedTimeFrame.value == 'Yearly') {
+      displayedData.assignAll(yearlyData);
+    }
+  }
+
+  void changeTimeFrame(String newTimeFrame) {
+    selectedTimeFrame.value = newTimeFrame;
+    updateDisplayedData();
+  }
 }
